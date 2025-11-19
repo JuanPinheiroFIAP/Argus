948b34f0c8581cf84b34f6adb01e3c69dca8861a
JuanPinheiroFIAP <rm552202@fiap.com.br>
2025-11-19 17:17:32 -0300

    Tls

--- src/main.py
+++ src/main.py
@@ -1,14 +1,33 @@
-import os 
-import pandas as pd 
+import os
+import ssl
 import requests
+from requests.adapters import HTTPAdapter
+from urllib3.poolmanager import PoolManager
 from dotenv import load_dotenv
+
+
+class TLSAdapter(HTTPAdapter):
+    def init_poolmanager(self, *args, **kwargs):
+        ctx = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
+        
+        ctx.set_ciphers('HIGH:!aNULL:!eNULL:!MD5:!RC4')
+        kwargs['ssl_context'] = ctx
+        return super(TLSAdapter, self).init_poolmanager(*args, **kwargs)
+
 
 load_dotenv()
 api_token = os.getenv("TOKEN")
+
 url = "https://argus.app.br/apiargus/report/tabulacoesdetalhadas?periodoInicial=2025-11-17&periodoFinal=2025-11-17&idCampanha=1"
 
-header = {
-    "Token-Signature" : api_token
+headers = {
+    "Token-Signature": api_token
 }
-response = requests.get(url, headers=header)
-print(response.json())
+
+session = requests.Session()
+session.mount("https://", TLSAdapter())
+
+response = session.get(url, headers=headers)
+
+print(response.status_code)
+print(response.text)
