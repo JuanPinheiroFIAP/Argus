6f1b225029f2df4a5fb53211b31b33e13d8fb925
JuanPinheiroFIAP <rm552202@fiap.com.br>
2025-11-26 18:32:25 -0300

    Arrumando a logica

--- src/main.py
+++ src/main.py
@@ -6,7 +6,8 @@ from dotenv import load_dotenv
 import json
 import logging
 import pandas as pd
-
+from datetime import datetime, timedelta
+import time
 
 # --- Variaveis de Ambiente ---
 load_dotenv()
@@ -15,13 +16,12 @@ api_token = os.getenv("TOKEN_API_ARGUS")
 path_log_file = "logs/argus.log"
 path_data_file = "data/argus.xlsx"
 
-data_inicial = "2025-11-10"
-data_final = ""
+
 
 
 # --- LOGGING ---
-logging.config(
-    filename="LOG_FILE",
+logging.basicConfig(
+    filename="logs/argus.log",
     level=logging.INFO,
     format="%(asctime)s [%(levelname)s] %(message)s",
 )
@@ -68,7 +68,7 @@ def fazer_requisicao(data_inicial, data_final):
     session = requests.Session()
     session.mount("https://", TLSAdapter())
 
-    response = session.get(url, headers=headers)
+    response = session.get(url, headers=headers, timeout=30)
     response.raise_for_status()
     dados = response.json()
     return dados
@@ -85,11 +85,30 @@ def salvar_arquivo(data):
         df.to_excel(path_data_file)
         pass
 
+
+resultado = []
 def main():
-    
-    pass
 
+# Se não existir o arquivo deve puxar da data 10/11/2025 até a data atual
+# se o arquivo já existir deve pegar a ultima data presente na base e fazer ela ate a data atual 
 
+    if not os.path.exists(path_data_file):
+        data_inicial = datetime.strftime("2025-11-10", "%Y-%m-%d")
+        data_final = data_inicial
+        while True:
+            try:
+                data = fazer_requisicao(data_inicial,data_final)
+                resultado.append(data)
+                data_inicial = data_inicial + timedelta(days=1)
+
+                if data.get('qtdeRegistros') <= 0:
+                    break
+                else:
+                    salvar_arquivo(resultado)
+
+            except Exception as e:
+                print("Deu ruim")
+        
 
 if __name__ == "__main__":
-    data = fazer_requisicao()
+    main()
